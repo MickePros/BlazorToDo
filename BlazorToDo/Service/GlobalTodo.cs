6b1d49e142d03909fdb7f9415ddcacdbe1659072
Mikael Pros <Mikael_Pros@hotmail.com>
2024-12-19 09:18:27 +0100

    Add Blazor Todo basic functionality

---
+++ BlazorToDo/Service/GlobalTodo.cs
@@ -0,0 +1,17 @@
+﻿namespace BlazorToDo.Service
+{
+    public class GlobalTodo
+    {
+        public List<TodoItem> todos = new();
+
+        public event Action? OnChange;
+
+        public void Add(TodoItem item)
+        {
+            todos.Add(item);
+            NotifyStateChanged();
+        }
+
+        public void NotifyStateChanged() => OnChange?.Invoke();
+    }
+}
